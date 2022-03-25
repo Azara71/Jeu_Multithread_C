1929f7c561070d8b7bc7eb3e41bcac8ca5f5278b
Azara71 <73836814+Azara71@users.noreply.github.com>
2022-03-25 20:44:19 +0100

    Delete COPIEPROJET directory

--- COPIEPROJET/entite.c
+++
@@ -1,8 +0,0 @@
-#include "entite.h"
-
-/*
-monstre_t delilosaure=create_monster("Delilosaure",1,1,1,1,1);
-entite_t entite_monstre;
-entite_monstre.monstre=delilosaure;
-printf("%s",entite_monstre.monstre.name);
-*/
